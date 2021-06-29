9165afcb45e9aa8088cd61941c36148115d18ad3
unknown <cjy71092298@gmail.com>
2021-06-29 09:14:17 +0900

    Updates

---
+++ precache-manifest.b1ef2d3a4aa6cec2cc13404d99209faa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a2be5e5b591f3db4226",
+    "url": "/portfolio/static/css/main.4a73abb1.chunk.css"
+  },
+  {
+    "revision": "9a2be5e5b591f3db4226",
+    "url": "/portfolio/static/js/main.9a2be5e5.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/portfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "0ee7491c83257c2b366c0270f182725c",
+    "url": "/portfolio/index.html"
+  }
+];
